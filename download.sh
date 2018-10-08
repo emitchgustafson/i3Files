6c413837df8ee15ec198d5526603f682cdaefa50
Erik Gustafson <emitchgustafson@gmail.com>
2018-10-08 00:48:23 -0400

    added bash rc and dir restructure

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cp ./fonts/*.otf ~/.local/share/fonts/
-cp -r i3 ~/.config/
+cp src/fonts/*.otf ~/.local/share/fonts/
+cp -r src/i3 ~/.config/
+cp -r src/ranger ~/.config
+cp src/bashrc ~/.bashrc
--- upload.sh
+++ upload.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cp ~/.local/share/fonts/*.otf ./fonts/
-cp -r ~/.config/i3 .
+cp ~/.local/share/fonts/*.otf src/fonts/
+cp -r ~/.config/i3 src/
+cp -r ~/.config/ranger src/
+cp ~/.bashrc src/bashrc
