16a1c4551ee6c76445d3bdf44585be6766f04fc5
Erik Gustafson <emitchgustafson@gmail.com>
2018-10-08 00:36:32 -0400

    ranger and autoremove to install script

--- i3install.sh
+++ i3install.sh
@@ -16,14 +16,22 @@ sudo apt-get install compton -y
 sudo apt-get install arandr -y
 sudo apt-get install i3blocks -y
 
+# Graphical interface for changing fonts and themes and icons and stuff
 sudo apt-get install lxappearance -y
 # Nautilius is breaking on 16.04 so installing this file explorer
 sudo apt-get install thunar -y
-# sudo apt-get remove nautilius
-# INstall icons required for thunar-if changing icons anyway you dont need this
+#sudo apt-get remove nautilius
+# Install icons required for thunar-if changing icons anyway you dont need this
 sudo apt-get install gnome-icon-theme-full -y
-sudo apt-get install scrot
+# Lock Screen
+sudo apt-get install scrot -y
+# Ranger
+sudo apt-get install ranger caca-utils highlight atool w3m poppler-utils mediainfoi -y
 
 #other folders to copy
 #~/.local/share/fonts/
+#~/.config/i3
+#~/.config/ranger
 
+# lets autoremove as a last command to clean things up
+sudo apt-get autoremove
