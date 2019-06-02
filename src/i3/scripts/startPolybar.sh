4eb196cdf8514c5972708aa337c54c4f7c1c8e13
mitch <emitchgustafson@gmail.com>
2019-06-02 19:14:52 -0400

    script automaticall starts polybar on all monitors

--- src/i3/scripts/startPolybar.sh
+++ src/i3/scripts/startPolybar.sh
@@ -2,9 +2,13 @@
 
 killall polybar &> /dev/null
 
-export MONITOR=VGA-1
-polybar mainbar &
-polybar topbar &
-export MONITOR=VGA-2
-polybar mainbar &
-polybar topbar &
+DISPS=$(xrandr | grep " connected" | cut -d ' ' -f1)
+
+for DISP in $DISPS
+do
+  echo "$DISP"
+  export MONITOR=$DISP
+  polybar mainbar &
+  polybar topbar &
+done
+
