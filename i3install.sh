21856db784d14d51ccbc956d77c9498d04cbb759
erik <emitchgustafson@gmail.com>
2020-07-21 17:40:59 -0400

    update for 2004

--- i3install.sh
+++ i3install.sh
@@ -9,18 +9,12 @@ echo "Updating and Upgrading"
 $update
 
 #install lightdm for ubuntu 18.04
-$install lightdm
+#$install lightdm
 
 #Install i3wm
 echo "Installing i3wm & config"
 $install i3 i3status dmenu i3lock xbacklight feh conky rofi
 $install arandr i3blocks scrot jq
-#Use XFCE panel instead of default i3 and xfce plugins
-#$install xfwm4 xfce4-statusnotifier-plugin xfce4-statusnotifier-plugin
-#$install xfce4-pulseaudio-plugin xfce4-sensors-plugin xfce4-battery-plugin
-#$install xfce4-panel xfce4-clipman-plugin xfce4-session xfce4-whiskermenu-plugin
-#$install xfce4-mount-plugin xfce4-kbdleds-plugin
-# Installs compton to prevent screen tearing
 sleep 2
 $install compton unclutter
 #hides mouse when not in use.
@@ -30,19 +24,6 @@ sleep 2
 mkdir /home/$user/.config/i3
 sleep 2
 
-# other programs
-# spotify
-# need keepass2
-$install keepass2
-
-# redshift
-$install redshift
-
-# Nautilius is breaking on 16.04 so installing this file explorer
-$install thunar
-# Install icons required for thunar-if changing icons anyway you don't need this
-$install gnome-icon-theme-full
-
 # Ranger
 $install ranger caca-utils highlight atool w3m poppler-utils mediainfo
 
@@ -50,12 +31,14 @@ $install ranger caca-utils highlight atool w3m poppler-utils mediainfo
 sudo apt-get -y autoremove
 
 # Polybar
-$install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev 
-$install libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config 
-$install python-xcbgen xcb-proto i3-wm libasound2-dev libmpdclient-dev 
-$install libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
-$install libxcb-xrm-dev # put this on seperate line because it doen't always exsist
-cd polybar && ./build.sh && cd ..
+$install \
+  cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
+  libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev \
+  libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen \
+  xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev \
+  libiw-dev libcurl4-openssl-dev libpulse-dev \
+  libxcb-composite0-dev xcb libxcb-ewmh2
+pushd polybar && ./build.sh && popd
 
 #load included config files
 ./load.sh
