57c69953cc762505b6794aa4c046a884a94e06e4
Erik Gustafson <emitchgustafson@gmail.com>
2023-12-21 16:23:27 -0500

    ad vscode installer

---
+++ install_tools/vscode.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt install code --classic
+
+code --install-extension alefragnani.Bookmarks
+code --install-extension alefragnani.project-manager
+code --install-extension chrmarti.regex
+code --install-extension donjayamanne.githistory
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.code-runner
+code --install-extension golang.go
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension jeff-hykin.better-cpp-syntax
+code --install-extension johnpapa.vscode-peacock
+code --install-extension KevinRose.vsc-python-indent
+code --install-extension mohsen1.prettify-json
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.hexeditor
+code --install-extension ms-vscode.remote-explorer
+code --install-extension oderwat.indent-rainbow
+code --install-extension redhat.vscode-xml
+code --install-extension redhat.vscode-yaml
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension tomoki1207.pdf
+code --install-extension twxs.cmake
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension yzhang.markdown-all-in-one
+
