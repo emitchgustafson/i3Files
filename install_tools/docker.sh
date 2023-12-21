60a2232caec165de7412ae9a161fe6c2535109a5
Erik Gustafson <emitchgustafson@gmail.com>
2023-12-21 13:46:59 -0500

    add docker images

--- install_tools/docker.sh
+++ install_tools/docker.sh
@@ -7,3 +7,8 @@ sudo usermod -aG docker $USER
 sudo docker pull ubuntu:18.04
 sudo docker pull ubuntu:20.04
 sudo docker pull ubuntu:22.04
+sudo docker pull debian:8
+sudo docker pull debian:9
+sudo docker pull debian:10
+sudo docker pull debian:11
+sudo docker pull debian:12
